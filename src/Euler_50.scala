3c450e321fa0db91c4739cb0f638428c47571a74
Mateusz Kapturski <fenix.citizen@gmail.com>
2016-05-03 22:37:27 +0100

    Demonstrated interop between Java and Scala

---
+++ src/Euler_50.scala
@@ -0,0 +1,15 @@
+/**
+ * Created by mateusz on 03/05/16.
+ */
+object Euler_50 extends EulerRunner {
+
+  def main(args: Array[String]) {
+    execute()
+  }
+
+  override protected def run(): String = {
+    println("Uruchomilem sie!")
+    "Wynik!"
+  }
+
+}
--- test/util/PrimesHelperTest.java
+++ test/util/PrimesHelperTest.java
@@ -1,6 +1,6 @@
 package util;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigInteger;
