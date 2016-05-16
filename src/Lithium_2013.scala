4307b7f85a85db0212d4ba288e02195198bc50e6
Mateusz Kapturski <fenix.citizen@gmail.com>
2016-05-16 22:49:03 +0100

    Speeded up solution - using String internal representation

--- src/Lithium_2013.scala
+++ src/Lithium_2013.scala
@@ -1,4 +1,7 @@
+import java.util
+
 import scala.collection.immutable.IndexedSeq
+import scala.reflect.ClassTag
 
 /**
  * Created by mateusz on 03/05/16.
@@ -6,6 +9,7 @@ import scala.collection.immutable.IndexedSeq
 object Lithium_2013 extends EulerRunner {
 
   def C: Array[Array[Int]] = Array(Array(1, 2, 3, 4, 5, 7, 9), Array(1, 2, 3, 5, 7, 8, 10), Array(1, 2, 3, 4, 5, 7, 9), Array(10, 1, 2, 3, 4, 7, 9), Array(10, 1, 2, 3, 4, 7, 9), Array(6, 8, 9, 10, 1, 2, 5), Array(7, 8, 9, 10, 1, 2, 5))
+
   def PP: Int = 10
 
   def main(args: Array[String]) {
@@ -17,7 +21,7 @@ object Lithium_2013 extends EulerRunner {
   }
 
   def solution(A: Array[Array[Int]], P: Int): Int = {
-    val clocks: Array[Spaces] = parseClocks(A,P)
+    val clocks: Array[Spaces] = parseClocks(A, P)
 
     val freqs: Set[(Spaces, Int)] = calculateFreqs(clocks)
 
@@ -26,7 +30,7 @@ object Lithium_2013 extends EulerRunner {
 
   def calculateFreqs(clocks: Array[Spaces]): Set[(Spaces, Int)] = {
     clocks.toSet
-      .map((cat: Spaces) => (cat, clocks.count((x: Spaces) => same(x.spaces, cat.spaces))))
+      .map((cat: Spaces) => (cat, clocks.count((x: Spaces) => x.equals(cat))))
   }
 
   def parseClocks(A: Array[Array[Int]], P: Int): Array[Spaces] = {
@@ -41,7 +45,7 @@ object Lithium_2013 extends EulerRunner {
         else
           space
       })
-    }).map(Spaces)
+    }).map((as: Array[Int]) => new Spaces(as))
   }
 
   def same(cat: Array[Int], setup: Array[Int]): Boolean = {
@@ -49,19 +53,35 @@ object Lithium_2013 extends EulerRunner {
     rotations.exists(rotation => cat sameElements rotation)
   }
 
-  def rotate[A](n: Int, l: Array[A])(implicit m:ClassManifest[A]): Array[A] = {
+  def rotate[A](n: Int, l: Array[A])(implicit m: ClassTag[A]): Array[A] = {
     val wrapn = if (l.isEmpty) 0 else n % l.length
     if (wrapn < 0) rotate(l.length + n, l)
     else l.drop(wrapn).++(l.take(wrapn))
   }
 
-  case class Spaces(spaces: Array[Int]) {
+  case class Spaces(spaces: Array[Int], representation: String) {
+
+    def this(spaces: Array[Int]) {
+      this(spaces, util.Arrays.toString((for {rotationSize <- spaces.indices} yield rotate(rotationSize, spaces)).min(new Ordering[Array[Int]] {
+        override def compare(xs: Array[Int], ys: Array[Int]): Int = {
+          require(xs.length == ys.length)
+          xs.zip(ys).foldLeft(0) { case (z, (x, y)) =>
+            if (z != 0)
+              z
+            else {
+              if (x < y) -1 else if (x == y) 0 else 1
+            }
+          }
+        }
+      }): Array[Int]))
+    }
+
     override def equals(o: Any) = o match {
-      case (that: Spaces) => same(this.spaces, that.spaces)
+      case (that: Spaces) => this.representation.equals(that.representation)
       case _ => false
     }
 
-    override def hashCode = 0
+    override def hashCode = this.representation.hashCode
   }
 
 }
